459c5c21eaa795ec51b38757da83bd76e6da05ab
Administrator <itadmin.erp@byondwave.com>
2026-03-11 04:21:57 +0000

    fix: use party_name instead of customer

--- alchemistic/doc_events/quotation.py
+++ alchemistic/doc_events/quotation.py
@@ -7,7 +7,5 @@ def autoname(doc: Quotation, action):
     transaction_date = getdate(doc.transaction_date)
     year = str(transaction_date.year)[-1]
     month = transaction_date.strftime("%m")
-    prefix = doc.customer[0].upper()
-    if doc.project:
-        prefix = doc.project[0].upper()
+    prefix = doc.party_name[0].upper()
     doc.naming_series = prefix + year + month + ".#"
